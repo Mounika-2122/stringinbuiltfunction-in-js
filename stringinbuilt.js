83e5432d1809ece68afcd7f98fffbe34e26182a0
Mounika <bojjamounika8@gmail.com>
2021-12-14 23:42:15 +0530

    stringinbuilt

---
+++ stringinbuilt.js
@@ -0,0 +1,45 @@
+var mystr="java script is a frontend and backend language";
+console.log(mystr.length);
+
+
+var myStr="java script is a frontend and backend language";
+console.log(myStr.indexOf('frontend'));
+
+var myStr="java script is a frontend and backend language"
+console.log(myStr.lastIndexOf('frontend'));
+
+var str= "javascript is a frontend language";
+console.log(str.slice(0,7));
+console.log(str.slice(-10));
+console.log(str.slice (8));
+
+var str= "javascript is a frontend language";
+console.log(str.substr(5,9));
+
+var myStr= "javascript is a frontend language";
+console.log(str.toUpperCase(myStr));
+
+
+var myStr= "JAVASCRIPT IS A FRONTEND LANGUAGE";
+console.log(str.toLowerCase(myStr));
+
+
+var firstStr="front";
+var secondStr="end";
+console.log(firstStr.concat(secondStr));
+console.log(firstStr.concat(' ',secondStr));
+console.log(firstStr.concat('@',secondStr));
+
+
+var myStr="   Hai   ";
+console.log(myStr.trim());
+
+
+var str="JAVASCRIPT IS A FRONTEND LANGUAGE";
+console.log(str.charAt(9));
+
+
+    var str="JAVASCRIPT IS A FRONTEND LANGUAGE";
+    console.log(str.split('',));
+    
+    
